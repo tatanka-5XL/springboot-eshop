02901b5e7fd4dbc4f0bf851efa9c487393a183a6
Petr Pribil <petrp@myteepee.local>
2022-07-22 19:15:46 +0200

    First commit - spring initialized

---
+++ src/main/java/cz/petrpribil/ita/ItaApplication.java
@@ -0,0 +1,13 @@
+package cz.petrpribil.ita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/petrpribil/ita/ItaApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.petrpribil.ita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
