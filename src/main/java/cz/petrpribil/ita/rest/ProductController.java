155528ae6a572432ed3e4ae41ae961719f67d546
Petr Pribil <petrp@myteepee.local>
2022-07-22 21:26:59 +0200

    First successful DTO created

--- src/main/java/cz/petrpribil/ita/rest/ProductController.java
+++ src/main/java/cz/petrpribil/ita/rest/ProductController.java
@@ -1,14 +1,20 @@
 package cz.petrpribil.ita.rest;
 
+import cz.petrpribil.ita.model.ProductDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
     @GetMapping("greeting")
-    public String test() {
-        return new ProductDto(){
-
-        };
+    public ProductDto test() {
+        return new ProductDto(
+                "Sekera",
+                "Super rychle seka, lehka a ucinna",
+                "https://www.mall.cz/i/45314226/550/550",
+                1500L,
+                5L,
+                1L
+        );
     }
 }
