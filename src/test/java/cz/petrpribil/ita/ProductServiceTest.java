e0527933c5341b1b8e5d73efa253d0314b0016b3
Petr Pribil <petrp@myteepee.local>
2022-07-31 18:33:56 +0200

    Test ammended - mock seems functional but test still not passing

--- src/test/java/cz/petrpribil/ita/ProductServiceTest.java
+++ src/test/java/cz/petrpribil/ita/ProductServiceTest.java
@@ -1,41 +1,29 @@
 package cz.petrpribil.ita;
 
-import cz.petrpribil.ita.domain.Product;
 import cz.petrpribil.ita.repository.ProductRepository;
-import cz.petrpribil.ita.rest.ProductController;
-import cz.petrpribil.ita.service.ProductService;
 import org.junit.jupiter.api.*;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 public class ProductServiceTest {
-        @BeforeEach
-    void callInit() {
-        productService.init();
-    }
+
+    private ProductRepository mockedProductRepository = mock(ProductRepository.class);
+
 
     @Test
     @DisplayName("Test if all the products are shown")
     void findAllProducts() {
-        assertThat(productService.findAllProducts())
-                .as("Check the map size")
-                .hasSize(3);
+        assertThat(mockedProductRepository.findAll())
+                .as("Check the product size")
+                .hasSize(2);
     }
 
     @Test
     @DisplayName("Test if product is shown, based on its id")
     void findProduct() {
-        assertThat(productService.findProduct(3L).getName())
-                .as("Check if the last product is Firesteel")
-                .isEqualTo("Firesteel");
+        assertThat(mockedProductRepository.findById(1L))
+                .as("Check if the first product is Sekera")
+                .isEqualTo("Sekera");
     }
 }
