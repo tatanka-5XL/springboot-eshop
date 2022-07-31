ea7a98691ffaee96fecb2992f80284bb03dcd7d3
Petr Pribil <petrp@myteepee.local>
2022-07-31 11:39:30 +0200

    ProductService updated using ProductServiceInterface

--- src/main/java/cz/petrpribil/ita/service/ProductService.java
+++ src/main/java/cz/petrpribil/ita/service/ProductService.java
@@ -1,11 +1,13 @@
 package cz.petrpribil.ita.service;
 
+import cz.petrpribil.ita.domain.Product;
 import cz.petrpribil.ita.model.ProductDto;
 import cz.petrpribil.ita.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityNotFoundException;
 import java.util.Collection;
 import java.util.Map;
 import java.util.function.Function;
@@ -19,10 +21,26 @@ public class ProductService implements ProductServiceInterface {
     private final ProductRepository productRepository;
 
     public ProductDto findProduct(Long id) {
-        return productDtoMap.get(id);
+        return productRepository.findById(id)
+        .map(this::mapToDto)
+                .orElseThrow(()-> new EntityNotFoundException("Product " + id + " not found!"));
     }
 
     public Collection<ProductDto> findAllProducts() {
-        return productDtoMap.values();
+        return productRepository.findAll().stream()
+                .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+
+    private ProductDto mapToDto(Product product) {
+        return new ProductDto(
+                product.getName(),
+                product.getDescription(),
+                product.getImage(),
+                product.getPrice(),
+                product.getStock(),
+                product.getId()
+        );
     }
 }
