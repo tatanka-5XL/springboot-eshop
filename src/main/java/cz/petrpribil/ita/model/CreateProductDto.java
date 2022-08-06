abfd228ac018e3047aafbedcce7ae6ad6c8aaa29
Petr Pribil <petrp@myteepee.local>
2022-08-06 21:05:48 +0200

    Validations completed

--- src/main/java/cz/petrpribil/ita/model/CreateProductDto.java
+++ src/main/java/cz/petrpribil/ita/model/CreateProductDto.java
@@ -5,15 +5,23 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class CreateProductDto {
     @NotBlank
+    @Size(max=256)
     private String name;
+    @NotBlank
+    @Size(max=512)
     private String description;
     private String image;
+    @NotNull
+    @Size
     private Long price;
+    @Size
     private Long stock;
 }
