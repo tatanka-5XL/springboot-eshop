cac899a4806d9426be0cf68900cdce6282270d16
Petr Pribil <petrp@myteepee.local>
2022-08-07 17:37:02 +0200

    Exceptions shall be functional now

--- src/main/java/cz/petrpribil/ita/model/CreateProductDto.java
+++ src/main/java/cz/petrpribil/ita/model/CreateProductDto.java
@@ -14,16 +14,13 @@ import javax.validation.constraints.Size;
 @Setter
 @AllArgsConstructor
 public class CreateProductDto {
-    @NotBlank
-    @Size(max=256)
-    @StartsWithUppercase(message = "Must start with an uppercase!")
+    @NotBlank @Size(max=256) @StartsWithUppercase(message = "Must start with an uppercase!")
     private String name;
-    @NotBlank
-    @Size(max=512)
+    @NotBlank @Size(max=512)
     private String description;
+    @NotBlank
     private String image;
-    @NotNull
-    @Range
+    @Range(min=1)
     private Long price;
     @Range
     private Long stock;
