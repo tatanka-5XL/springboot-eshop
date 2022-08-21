6d66200ab89653a67eaf271cb51e893dececb571
Petr Pribil <petrp@myteepee.local>
2022-08-21 16:21:17 +0200

    lombok.config added, Manufacturer in domain added

---
+++ src/main/java/cz/petrpribil/ita/domain/Manufacturer.java
@@ -0,0 +1,14 @@
+package cz.petrpribil.ita.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@Accessors(chain = true)
+public class Manufacturer {
+}
