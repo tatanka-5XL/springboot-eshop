7f00b980ee0bdca9f42cb1da63a6822fb48a5e4a
Petr Pribil <petrp@myteepee.local>
2022-08-21 16:38:54 +0200

    manufacturer added

--- src/main/java/cz/petrpribil/ita/domain/Manufacturer.java
+++ src/main/java/cz/petrpribil/ita/domain/Manufacturer.java
@@ -3,10 +3,22 @@ package cz.petrpribil.ita.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 public class Manufacturer {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String about;
+    private String vatNr;
+    @ManyToOne
+    @JoinColumn (
+            name = "id_manufacturer"
+    )
+    private Manufacturer manufacturer;
+
 }
