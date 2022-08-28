c0311c58844352439089867dae4c91ed198ad152
Petr Pribil <petrp@myteepee.local>
2022-08-28 18:45:29 +0200

    OrderRepositoryIT finished but not passing

--- src/test/java/cz/petrpribil/ita/repository/OrderRepositoryIT.java
+++ src/test/java/cz/petrpribil/ita/repository/OrderRepositoryIT.java
@@ -1,6 +1,7 @@
 package cz.petrpribil.ita.repository;
 
-import cz.petrpribil.ita.domain.Cart;
+import cz.petrpribil.ita.domain.Order;
+import cz.petrpribil.ita.repository.OrderRepository;
 import cz.petrpribil.ita.domain.Product;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
@@ -14,32 +15,33 @@ import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
-class CartRepositoryIT implements WithAssertions {
+class OrderRepositoryIT implements WithAssertions {
+
 
     @Autowired
-    private CartRepository cartRepository;
+    private OrderRepository orderRepository;
 
     @Autowired
     private TestEntityManager testEntityManager;
 
     @Test
-    void testCreateAndRetrieveCart(){
+    void testCreateAndRetrieveOrder(){
 
         List<Product> testProducts = List.of(getTestProduct(), getTestProduct());
 
-        Cart testCart = new Cart()
+        Order testOrder = new Order()
                 .setProducts(testProducts);
 
-        testEntityManager.persistAndFlush(testCart);
-        testEntityManager.detach(testCart);
+        testEntityManager.persistAndFlush(testOrder);
+        testEntityManager.detach(testOrder);
 
-        Optional<Cart> result = cartRepository.findById(testCart.getId());
+        Optional<Order> result = orderRepository.findById(testOrder.getId());
 
         assertThat(result).isNotEmpty();
 
-        Cart resultCart = result.get();
+        Order resultOder = result.get();
 
-        assertThat(resultCart.getId()).isEqualTo(testCart.getId());
-        assertThat(resultCart.getProducts()).isEqualTo(testCart.getProducts());
+        assertThat(resultOder.getId()).isEqualTo(testOrder.getId());
+        assertThat(resultOder.getProducts()).isEqualTo(testOrder.getProducts());
     }
 }
