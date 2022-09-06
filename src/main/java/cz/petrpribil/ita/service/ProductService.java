8097a01d4acbf0eccab3401ffed3138ae8249e04
Petr Pribil <petrp@myteepee.local>
2022-09-06 20:17:05 +0200

    Javadocs in ProductService done

--- src/main/java/cz/petrpribil/ita/service/ProductService.java
+++ src/main/java/cz/petrpribil/ita/service/ProductService.java
@@ -7,13 +7,38 @@ import cz.petrpribil.ita.model.ProductSimpleDto;
 import java.util.Collection;
 
 public interface ProductService {
+
+    /**
+     * To get complete product info based on its Id, and return it back to API
+     * @param id
+     * @return product as {@link ProductDto}
+     */
     ProductDto findProduct(Long id);
 
+    /**
+     * To get collection of all products a return it back to API
+     * @return products as {@link Collection<ProductSimpleDto>}
+     */
     Collection<ProductSimpleDto> findAllProducts();
 
+    /**
+     * To insert a new product into the database
+     * @param productDto
+     * @return new product as {@link ProductDto}
+     */
     ProductDto createProduct(ProductRequestDto productDto);
 
+    /**
+     * To change any of product parameters and save that updated product
+     * @param id
+     * @param productDto
+     * @return updated product as {@link ProductDto}
+     */
     ProductDto updateProduct(Long id, ProductRequestDto productDto);
 
+    /**
+     * To remove complete information about given product
+     * @param id
+     */
     void deleteProduct(Long id);
 }
